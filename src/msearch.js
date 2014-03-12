702bc9b375f15a48381977ef431148cc1e0da6b4
Dca <dca35311@gmail.com>
2014-03-12 13:54:55 +0800

    simple js

---
+++ src/msearch.js
@@ -0,0 +1,27 @@
+(function($){
+    $.fn.search = function(selector, field){
+        if (!selector) throw new Error('need a selector');
+
+        field = field || 'data-search';
+
+        return this.each(function(idx, dom){
+            var styleWarp;
+
+            styleWarp = $('<style class="styleWarp"></style>');
+            styleWarp.appendTo('body');
+
+            $(this).on('change paste keyup', function onSearch(){
+                var search = $(this).val();
+                var style = '';
+
+                if (search){
+                    style = selector + ':not([' + field + '*="' + search.toLowerCase() + '"]) {display: none;}';
+                }
+                styleWarp.text(style);
+            });
+
+            return this;
+        });
+    }
+})(jQuery);
+
